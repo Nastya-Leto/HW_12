243d098c73acfefdf5fcc6ca736cf634314288f7
Anastasiya <soidberg91@gmail.com>
2023-03-30 09:10:42 +0300

    add steps

--- src/test/java/leto/nastya/pages/FormTest.java
+++ src/test/java/leto/nastya/pages/FormTest.java
@@ -3,6 +3,8 @@ package leto.nastya.pages;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+import static io.qameta.allure.Allure.step;
+
 
 public class FormTest extends TestData {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -10,34 +12,43 @@ public class FormTest extends TestData {
     @Test
     void fillFormTest() {
 
-        new RegistrationPage().openPage()
-                .closeBanner()
-                .setFirstName(userName)
-                .setLastName(userLastName)
-                .setEMail(email)
-                .setGender(gender)
-                .setPhone(phone)
-                .setBirthDate(userBirthDate, userBirthMonth, userBirthYear)
-                .setSubjects(userSubjects)
-                .setHobbies(userHobbies)
-                .setAddress(address)
-                .setState(userState)
-                .setCity(userCity)
-                .addFile(file)
-                .setSubmit();
+        step("Open form", () -> {
+            new RegistrationPage().openPage()
+                    .closeBanner();
+        });
+
+        step("Fill form", () -> {
+            new RegistrationPage().setFirstName(userName)
+                    .setLastName(userLastName)
+                    .setEMail(email)
+                    .setGender(gender)
+                    .setPhone(phone)
+                    .setBirthDate(userBirthDate, userBirthMonth, userBirthYear)
+                    .setSubjects(userSubjects)
+                    .setHobbies(userHobbies)
+                    .setAddress(address)
+                    .setState(userState)
+                    .setCity(userCity)
+                    .addFile(file)
+                    .setSubmit();
+        });
 
 
-        registrationPage.verifyResultsModalAppears()
-                .verifyResults("Student Name", userLastName + " " + userName)
-                .verifyResults("Student Email", email)
-                .verifyResults("Gender", gender)
-                .verifyResults("Mobile", phone)
-                .verifyResults("Date of Birth", userBirthDate + " " + userBirthMonth + "," + userBirthYear)
-                .verifyResults("Subjects", userSubjects)
-                .verifyResults("Hobbies", userHobbies)
-                .verifyResults("Picture", "img1.png")
-                .verifyResults("Address", address)
-                .verifyResults("State and City", userState + " " + userCity);
+        step("Verify results", () -> {
+            registrationPage.verifyResultsModalAppears()
+                    .verifyResults("Student Name", userLastName + " " + userName)
+                    .verifyResults("Student Email", email)
+                    .verifyResults("Gender", gender)
+                    .verifyResults("Mobile", phone)
+                    .verifyResults("Date of Birth", userBirthDate + " " + userBirthMonth + "," + userBirthYear)
+                    .verifyResults("Subjects", userSubjects)
+                    .verifyResults("Hobbies", userHobbies)
+                    .verifyResults("Picture", "img1.png")
+                    .verifyResults("Address", address)
+                    .verifyResults("State and City", userState + " " + userCity);
+
+        });
+
 
     }
 }
